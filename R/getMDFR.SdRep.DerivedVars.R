713d72f4d38b22fb37cfc8a5b93591bee40f2974
William Stockhausen <william.stockhausen@noaa.gov>
2022-08-29 11:59:31 -0700

    2022.08.29. Added/fixed getMDFR.SdRep functions.

---
+++ R/getMDFR.SdRep.DerivedVars.R
@@ -0,0 +1,62 @@
+#'
+#' @title Get sd_report info on derived quantities as a dataframe
+#'
+#' @description Function to get sd_report info on derived time series estimates as a dataframe.
+#'
+#' @param tcsams - a tcsam02.resLst object or named list of such
+#' @param ci - confidence interval
+#' @param verbose - flag to print debugging info
+#'
+#' @return a dataframe
+#'
+#' @details Derived variables are 
+#' "sdrAvgRec","sdrBmsy","sdrFmsy","sdrMSY","sdrCurB","sdrPrjB","sdrOFL"
+#'
+#' @import magrittr
+#' @importFrom dplyr mutate
+#' @importFrom dplyr select
+#'
+#' @export
+#'
+getMDFR.SdRep.DerivedVars<-function(tcsams,
+                                    ci=0.95,
+                                    verbose=FALSE){
+  if (inherits(tcsams,'tcsam02.resLst')){
+      #tcsams is a tcsam02 resLst object
+      #do nothing and drop out of "if" statement
+  } else if (inherits(tcsams,'list')){
+      #tcsams should be a list of tcsam02.resLst objects
+      mdfr<-NULL;
+      nl<-length(tcsams);
+      nms<-names(tcsams);
+      for (l in 1:nl){
+          if (verbose) cat("Processing",nms[l],"\n")
+          tcsam1<-tcsams[[l]];
+          mdfrp<-getMDFR.SdRep.DerivedVars(tcsams=tcsam1,verbose=verbose);
+          if (!is.null(mdfrp)){
+              if (!is.null(nms[l])) mdfrp$case<-nms[l];
+              mdfr<-rbind(mdfr,mdfrp);
+          }
+      }
+      if (verbose) cat("--finished rTCSAM02::getMDFR.SdRep.DerivedVars().\n");
+      return(mdfr);
+  } else {
+      cat("Error in getMDFR.SdRep.DerivedVars(tcsams).\n")
+      cat("'tcsams' should be a 'tcsam02.resLst' object or a list of such.\n")
+      cat("Returning NULL.\n")
+      return(NULL);
+  }
+
+  #--extract variables info
+  vars = c("sdrAvgRec","sdrBmsy","sdrFmsy","sdrMSY","sdrCurB","sdrPrjB","sdrOFL");
+  #----extract std info
+  dfrStd = tcsams$std;
+  dfr    = dfrStd %>% subset(name %in% vars) %>% dplyr::select(!one_of("row id")) %>%
+                      dplyr::mutate(variable=stringr::str_sub(name,4,-1),
+                                    y="all",x="all") %>% 
+                      dplyr::select(variable,y,x,name,est,`std.dev`) %>%
+                      dplyr::mutate(lci=qnorm(  (1-ci)/2,mean=est,sd=`std.dev`),
+                                    uci=qnorm(1-(1-ci)/2,mean=est,sd=`std.dev`));
+
+ return(dfr);
+}
--- R/getMDFR.SdRep.RecAndSSB.R
+++ R/getMDFR.SdRep.RecAndSSB.R
@@ -4,11 +4,12 @@
 #' @description Function to get sd_report info on recruitment and SSB time series estimates as a dataframe.
 #'
 #' @param tcsams - a tcsam02.resLst object or named list of such
+#' @param ci - confidence interval
 #' @param verbose - flag to print debugging info
 #'
 #' @return a dataframe
 #'
-#' @details Recruitment values are converted to arithmetic scale from ln-scale information.
+#' @details Recruitment values are on ln-scale.
 #'
 #' @import magrittr
 #' @importFrom dplyr mutate
@@ -17,6 +18,7 @@
 #' @export
 #'
 getMDFR.SdRep.RecAndSSB<-function(tcsams,
+                                  ci=0.95,
                                   verbose=FALSE){
   if (inherits(tcsams,'tcsam02.resLst')){
       #tcsams is a tcsam02 resLst object
@@ -52,17 +54,20 @@ getMDFR.SdRep.RecAndSSB<-function(tcsams,
   dfrStd<-tcsams$std;
   #----extract recruitment
   dfrRec<-dfrStd %>% subset(name=="sdrLnR_y") %>% dplyr::select(!one_of("row id"));
-  dfrRec<-cbind(data.frame(variable="Recruitment",y=y$vls[1:(ny-1)],x="all",stringsAsFactors=FALSE),dfrRec);
+  dfrRec<-cbind(data.frame(variable="lnRec",y=y$vls[1:(ny-1)],x="all",stringsAsFactors=FALSE),dfrRec);
   dfrRec<-dfrRec %>% dplyr::mutate(y=y+1);     #change rec year to year it enters population
-  ar.est<-exp(dfrRec$est);                     #convert ln-scale recruitment estimates to arithmetic-scale
-  ar.sdv<-sqrt(exp(dfrRec$std.dev^2)-1)*ar.est;#convert ln-scale recruitment sdvs to arithmetic-scale
-  dfrRec<-dfrRec %>% dplyr::mutate(est=ar.est,std.dev=ar.sdv);
+  dfrRec %<>% dplyr::mutate(lci=qnorm(  (1-ci)/2,mean=est,sd=`std.dev`),
+                            uci=qnorm(1-(1-ci)/2,mean=est,sd=`std.dev`));
+  # ar.est<-exp(dfrRec$est);                     #convert ln-scale recruitment estimates to arithmetic-scale
+  # ar.sdv<-sqrt(exp(dfrRec$std.dev^2)-1)*ar.est;#convert ln-scale recruitment sdvs to arithmetic-scale
+  # dfrRec<-dfrRec %>% dplyr::mutate(est=ar.est,std.dev=ar.sdv);
   #----extract SSB
   yv<-c(y$vls[1:(ny-1)],y$vls[1:(ny-1)]);
   xv<-c(rep("male",times=ny-1),rep("female",times=ny-1));
   dfrSSB<-dfrStd %>% subset(name=="sdrSpB_xy") %>% dplyr::select(!one_of("row id"));
   dfrSSB<-cbind(data.frame(variable="SSB",y=yv,x=xv),dfrSSB);
-  dfrSSB<-dfrSSB %>% subset(y<as.numeric(modYearName));
+  dfrSSB %<>% dplyr::mutate(lci=qnorm(  (1-ci)/2,mean=est,sd=`std.dev`),
+                            uci=qnorm(1-(1-ci)/2,mean=est,sd=`std.dev`));
 
   dfr<-rbind(dfrRec,dfrSSB);
  return(dfr);
